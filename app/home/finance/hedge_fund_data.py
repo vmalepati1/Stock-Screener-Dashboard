5b912cf5332f35a0365d5c34af78a768025443bf
vmalepati1 <malepativikas@gmail.com>
2021-01-22 12:55:41 -0500

    Fixed dynamic loading of hedge fund page

--- app/home/finance/hedge_fund_data.py
+++ app/home/finance/hedge_fund_data.py
@@ -18,38 +18,43 @@ def getMostBoughtData():
     # parses the html of the website
     page_soup = soup(page_hmtl, "html.parser")
 
-    n_symbols = 30
-
-    tickers_start_idx = 15
-    names_start_idx = 0
-    price_start_idx = 0
-    changes_start_idx = 2
-    volume_start_idx = 0
-    avg_volume_start_idx = 0
-
-    tickers_html = page_soup.findAll("a", {"class":"Fw(b)"})
+    # finds all the parts of the webpage that would hold the titles
+    symbols = page_soup.findAll("h2",{"class":"Fz(m)"})
+    tickers = page_soup.findAll("a", {"class":"Fw(b)"})
     prices_html = page_soup.findAll("td", {"class": "data-col2 Ta(end) Pstart(10px) Pend(6px) Fw(b)"})
     changes_html = page_soup.findAll("td", {"class": "data-col4 Ta(end) Pstart(10px) Pend(6px)"})
     volumes_html = page_soup.findAll("td", {"class": "data-col6 Ta(end) Pstart(10px) Pend(6px)"})
     avg_volumes_html = page_soup.findAll("td", {"class": "data-col7 Ta(end) Pstart(10px) Pend(6px)"})
 
-    data = []
+    sym = int(symbols[1].text[0:2])
 
-    for i in range(n_symbols):
-        ticker = tickers_html[tickers_start_idx + i]["title"]
-        price = prices_html[price_start_idx + i].text
+    x = 0
+
+    data = []
+    
+    while x < 48-1:
+        if len(tickers[x].text) > 5 or tickers[x].text == "Tech" or tickers[x].text == "News":
+            x = x + 1
+        else:
+            break
+    y = x
+    while x < y + sym:
+        ticker = tickers[x].text
+        price = prices_html[x - (y + sym)].text
         
-        change_str = changes_html[changes_start_idx + i].text
-        
+        change_str = changes_html[x - (y + sym)].text
+
         if change_str.startswith('+'):
             change = change_str[1:]
         else:
             change = change_str
-            
-        volume = volumes_html[volume_start_idx + i].text
-        avg_volume = avg_volumes_html[avg_volume_start_idx + i].text
+        
+        volume = volumes_html[x - (y + sym)].text
+        avg_volume = avg_volumes_html[x - (y + sym)].text
 
         data.append((ticker, price, change, volume, avg_volume))
+        
+        x = x + 1
 
     return data
 
